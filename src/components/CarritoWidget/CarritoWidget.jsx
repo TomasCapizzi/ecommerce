979f8f6d84e0de47be6a16229b7b185c64b25767
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-09-14 14:28:14 -0300

    Indent component

--- src/components/CarritoWidget/CarritoWidget.jsx
+++ src/components/CarritoWidget/CarritoWidget.jsx
@@ -1,16 +1,18 @@
 import React,{useContext} from 'react';
+
 import {AiOutlineShoppingCart} from 'react-icons/ai';
-import {Link} from 'react-router-dom';
 import {CartContext} from '../../Store/CartContext';
+import {Link} from 'react-router-dom';
 
 export default function CarritoWidget(){
     const {cart} = useContext(CartContext);
     return(
         <div className='carrito-container'>
             <Link to='/cart'><button className="carrito-btn"><AiOutlineShoppingCart /></button>
-            { cart.length > 0 && (
-            <div className='cart-widget'><p>{cart.length}</p></div>
-        )}</Link>
+                { cart.length > 0 && (
+                    <div className='cart-widget'><p>{cart.length}</p></div>
+                )}
+            </Link>
         </div>
     )
 }
