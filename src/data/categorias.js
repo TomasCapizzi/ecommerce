0e892a075d191138f67cab047ac4be16c1af5150
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-09-14 14:21:23 -0300

    Change location to new folder

---
+++ src/data/categorias.js
@@ -0,0 +1,12 @@
+export const categorias = [
+    {
+        id:1,
+        nombre: 'Clásicas'
+    },{
+        id:2,
+        nombre: 'Lupuladas'
+    },{
+        id:3,
+        nombre: 'Experimentales'
+    }
+]
